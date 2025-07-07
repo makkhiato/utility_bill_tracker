9f97762c373b7f8dac71640b88454645b56cc66c
Marco Bautista <marcobautista2836@gmail.com>
2025-07-07 23:58:30 +0800

    fix: correct billing_date typographical error

--- app/routes/bill_routes.py
+++ app/routes/bill_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from datetime import datetime
 from app.models import Bill, User
 from app.extensions import db
 
@@ -14,7 +15,7 @@ def get_bills():
             'id': bill.id,
             'utility_type': bill.utility_type,
             'amount': bill.amount,
-            'billing_data': bill.billing_date.isoformat(),
+            'billing_date': bill.billing_date.isoformat(),
             'due_date': bill.due_date.isoformat(),
             'status': bill.status
         }
@@ -27,12 +28,18 @@ def create_bill():
 
     data = request.get_json()
 
+    billing_date_str = data.get('billing_date')
+    due_date_str = data.get('due_date')
+
+    billing_date = datetime.strptime(billing_date_str, '%Y-%m-%d').date()
+    due_date = datetime.strptime(due_date_str, '%Y-%m-%d').date()
+
     bill = Bill(
         user_id=user.id,
         utility_type=data.get('utility_type'),
         amount=data.get('amount'),
-        billing_data=data.get('billing_date'),
-        due_date=data.get('due_date'),
+        billing_date=billing_date,
+        due_date=due_date,
         status=data.get('status','unpaid')
     )
 
@@ -61,8 +68,21 @@ def update_bill(bill_id):
 
     bill.utility_type = data.get('utility_type', bill.utility_type)
     bill.amount = data.get('amount', bill.amount)
-    bill.billing_date = data.get('billing_date', bill.billing_date)
-    bill.due_date = data.get('due_date', bill.due_date)
+    # bill.billing_date = data.get('billing_date', bill.billing_date)
+    # bill.due_date = data.get('due_date', bill.due_date)
+
+    if 'billing_date' in data:
+        try:
+            bill.billing_date = datetime.strptime(data['billing_date'], '%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({'error': 'Invalid billing_date format. Use YYYY-MM-DD'}), 400
+
+    if 'due_date' in data:
+        try:
+            bill.due_date = datetime.strptime(data['due_date'], '%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({'error': 'Invalid due_date format. Use YYYY-MM-DD'}), 400
+
     bill.status = data.get('status', bill.status)
 
     db.session.commit()
@@ -74,4 +94,4 @@ def delete_bill(bill_id):
     bill = Bill.query.get_or_404(bill_id)
     db.session.delete(bill)
     db.session.commit()
-    return jsonify({'message':'BIll deleted successfully'}), 200
+    return jsonify({'message':'Bill deleted successfully'}), 200

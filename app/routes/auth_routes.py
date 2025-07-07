97cdd1b3f5bfd916c6d2cfc6bbab4a5bfc2217d3
Marco Bautista <marcobautista2836@gmail.com>
2025-07-07 15:11:22 +0800

    feat: implement user registration and login routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,3 +1,43 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.models import User
+from app.extensions import db
 
 auth_bp = Blueprint('auth',__name__)
+
+@auth_bp.routes('/register',methods=['POST'])
+def register():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+
+    if not name or not email or not password:
+        return jsonify({'error':'All fields are required'}),400
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'error':'Email already registered'}),409
+
+    new_user = User(name=name, email=email)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message':'User registered successfully'}),201
+
+@auth_bp.route('/login',methods=['POST'])
+def login():
+    data = request.get_json()
+
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({'error':'Email and password required'}),400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or  not user.check_password(password):
+        return jsonify({'error':'Invalid credentials'}), 401
+
+    return jsonify({'message':'Login successful'}), 200
